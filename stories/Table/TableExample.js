9b6f1357d9b90f8457998ced0a49082a2f4a3ad2
Erez Makavy <erez@wix.com>
2018-06-28 15:44:36 +0300

    TableExample - add `Filter By` Dropdown to Toolbar

--- stories/Table/TableExample.js
+++ stories/Table/TableExample.js
@@ -32,6 +32,13 @@ export class TableExample extends React.Component {
       {id: 1, value: 'Towels'},
       {id: 2, value: 'Slippers'}
     ];
+
+    const filterOptions = [
+      {id: 0, value: 'All'},
+      {id: 1, value: 'Red'},
+      {id: 2, value: 'Cyan'}
+    ];
+
     return (
       <TableToolbar>
         <ItemGroup position="start">
@@ -41,8 +48,16 @@ export class TableExample extends React.Component {
           <Item>
             <Label>
               Collection
-              <span style={{maxWidth: '150px'}}>
+              <span style={{width: '150px'}}>
                 <Dropdown options={collectionOptions} selectedId={0} roundInput/>
+              </span>
+            </Label>
+          </Item>
+          <Item>
+            <Label>
+              Filter By
+              <span style={{width: '86px'}}>
+                <Dropdown options={filterOptions} selectedId={0} roundInput/>
               </span>
             </Label>
           </Item>
